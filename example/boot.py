f6c92f3fa7eafc3e637ef099ef2e652a98c8e3c8
Yoonseok Hur <yoonseok@gmail.com>
2023-01-11 16:57:20 +0900

    Renaming the module

--- example/boot.py
+++ example/boot.py
@@ -1,14 +1,14 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-#import webrepl
-#webrepl.start()
-#		In order to enable the webrepl, you need to import this and run the setup once
-#			import webrepl_setup
-import CommMgr
+# import esp
+# esp.osdebug(None)
+# import webrepl
+# webrepl.start()
+# In order to enable the webrepl, you need to import this and run the setup once
+# import webrepl_setup
+import ComMgr
 
-ble = CommMgr.startBLE('esp32s3')
-nic = CommMgr.startWiFi('esp32s3')
+ble = ComMgr.startBLE('esp32s3')
+nic = ComMgr.startWiFi('esp32s3')
 
 if nic:
     import esp
